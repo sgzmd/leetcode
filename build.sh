65bdee51460907c0313091bb6d56301730687afc
Roman Kirillov <sigizmund@gmail.com>
2024-04-03 14:22:57 +0100

    Fiddling with build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+cmake -B cmake-build-debug
 cmake --build ./cmake-build-debug
--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 cmake --build ./cmake-build-debug --target clean
+rm -rf ./cmake-build-debug ./build
